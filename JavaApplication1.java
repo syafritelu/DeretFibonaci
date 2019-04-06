3ec9601aa25e26dfa7b383182f435e7b94c6c532
syafritelu <49330123+syafritelu@users.noreply.github.com>
2019-04-06 10:57:08 +0700

    Master Commit File

---
+++ JavaApplication1.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       Scanner show = new Scanner(System.in);
+        System.out.print("Masukan Jumlah Deret Fibonacci : ");
+        int n = show.nextInt();
+        long fib[] = new long[n];
+         
+        fib[0] = 0;
+        fib[1] = 1;
+         
+        for(int i = 2; i < n; i++) {
+            fib[i] = fib[i-2] + fib[i-3];
+        }
+         
+        for (int i = 0; i < n; i++) {
+            System.out.print(fib[i] +  " ");
+        }
+    }
+    
+}
